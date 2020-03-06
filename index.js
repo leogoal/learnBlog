6f9edc753a5c9eb8c97d2b87a94a03188a6d2b66
lihao <1486600108@qq.com>
2020-03-06 20:22:05 +0800

    1

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ app.use(session({
   resave: true, // 强制更新 session
   saveUninitialized: false, // 设置为 false，强制创建一个 session，即使用户未登录
   cookie: {
-    maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
+    maxAge: config.session.maxAge,// 过期时间，过期后 cookie 中的 session id 自动删除
+    httpOnly: false
   },
   store: new MongoStore({// 将 session 存储到 mongodb
     url: config.mongodb// mongodb 地址
