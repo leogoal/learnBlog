b1966a73f8354e7c9137f12d27cb042deb0f7a0b
lihao <1486600108@qq.com>
2020-10-14 15:17:37 +0800

    数据库设置权限

--- config/default.js
+++ config/default.js
@@ -5,7 +5,7 @@ module.exports = {
     key: 'myblog',
     maxAge: 2592000000
   },
-  mongodb: 'mongodb://localhost:27017/myblog10',
+  mongodb: 'mongodb://root:messilihao10@localhost:27017/myblog10',
   tags: [
     "H5CQ",
     "前端",
